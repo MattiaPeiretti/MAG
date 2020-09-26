1a84f7420ae8a5146a1f074d0bf3171d1c833691
Mattia Peiretti <mattia.peiretti@gmail.com>
2020-09-26 15:13:15 +0200

    🙏Integrartion of the main class with the api and gui for testing purposes

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import constants
 import api
+import gui as GUI
 
 class MAG:
     def __init__(self):
         imageRetriever = api.ImageRetriever('')
+        gui = GUI.MAG_GUI()
+        gui.mainloop()
         print(imageRetriever.set_query('curiosity', 1099))
         imageRetriever.run()
+        print(imageRetriever.images())
 
 def main():
     mag = MAG()
